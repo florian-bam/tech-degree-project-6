bd2ce5d5ebc4652fb90504f34426a35e62903a25
Florian Gröne <florian@andbam.co>
2021-04-18 21:55:31 +0200

    I set up the getRandomPhrasesAsArray() function

--- app.js
+++ app.js
@@ -15,8 +15,24 @@ let phrases = [
 ];
 
 // Functions
-function getRandomPhraseAsArray(arr) {
-    
+
+// get a Math.random function to select a random number from 0 - arr.length
+function randomEngine (arr = phrases) {
+    let randomNumber = Math.floor(Math.random() * arr.length);
+    return randomNumber;
+}
+
+function getRandomPhraseAsArray(arr = phrases) {
+    // Choose randomly a phrase + save that randomly selected phrase in a variable
+    let randomPhrase = arr[randomEngine(arr)];
+    // Split that phrase into a new array of characters
+    let phraseLetters = [];
+    phraseLetters = randomPhrase.split("");
+    // for (let i = 0; i < randomPhrase.length; i++) {
+    //     // loop through every letter and store it in an array
+    // }
+    // // return the new character array
+    return phraseLetters;
 }
 
 // Event Listeners
